2e206af6ed93011e17efba9d83517bfa32861362
Jacob Corum <cor15031@byui.edu>
2020-09-12 18:21:06 -0600

    color scheme added and jumbotron

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "./App.css";
+import Hero from "./components/Jumbotron";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Nav from "./components/Nav";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div>
       <Nav />
       <main>
+        <Hero />
         <About />
         <Projects />
         <Resume />
+        <Contact />
       </main>
     </div>
   );
