56a2cedda412df18b6625da6c9671798d6d9800b
Jacob Corum <cor15031@byui.edu>
2020-09-14 16:54:15 -0600

    Updates

---
+++ precache-manifest.dbe0f73d2fdf7ab02d2c131ee3897525.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5e4d606871a466001efc4ded2ab8a6",
+    "url": "/dossier-vol-2/index.html"
+  },
+  {
+    "revision": "92f9f127adb2cd2b8c12",
+    "url": "/dossier-vol-2/static/css/main.4c377b6f.chunk.css"
+  },
+  {
+    "revision": "9750fc62812d9d94641f",
+    "url": "/dossier-vol-2/static/js/2.6ac6be55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dossier-vol-2/static/js/2.6ac6be55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f9f127adb2cd2b8c12",
+    "url": "/dossier-vol-2/static/js/main.41273f30.chunk.js"
+  },
+  {
+    "revision": "e4399a415a04850b6d95",
+    "url": "/dossier-vol-2/static/js/runtime-main.e137d3f9.js"
+  },
+  {
+    "revision": "07232a47bf18844af027ac336a297a3c",
+    "url": "/dossier-vol-2/static/media/IMG_0379.07232a47.jpg"
+  },
+  {
+    "revision": "5d119f12e70834f67d5535273edb7040",
+    "url": "/dossier-vol-2/static/media/README-zing.5d119f12.png"
+  },
+  {
+    "revision": "dd4126679dfaddd69a4e7ecbb97e32d3",
+    "url": "/dossier-vol-2/static/media/d&dTracker.dd412667.png"
+  },
+  {
+    "revision": "a64430dda34d8d03492ee009083745b7",
+    "url": "/dossier-vol-2/static/media/resume.a64430dd.png"
+  },
+  {
+    "revision": "ce1bb0a9c41db2a6485fe7cec3dda3e1",
+    "url": "/dossier-vol-2/static/media/resumeApr2020.ce1bb0a9.pdf"
+  },
+  {
+    "revision": "0fd3ed29e53735a44224715504d31a36",
+    "url": "/dossier-vol-2/static/media/run-buddy.0fd3ed29.png"
+  },
+  {
+    "revision": "18875e87759f983837de06ba7c6c87d9",
+    "url": "/dossier-vol-2/static/media/storm-ly.18875e87.png"
+  },
+  {
+    "revision": "c14d0ecf62778de8d52b9225fa32cfe5",
+    "url": "/dossier-vol-2/static/media/swap-squad.c14d0ecf.png"
+  },
+  {
+    "revision": "391f7c1dc28297218aa63c6d91c4f423",
+    "url": "/dossier-vol-2/static/media/test-rabbit.391f7c1d.png"
+  }
+]);
