8f05e137c62ef86df3149161bbb5b0854d882174
SteadyJingo <jacob.w.corum@gmail.com>
2022-02-10 11:22:07 -0700

    navbar styled to fit mock and functionality added to buttons so that when clicked they will jump to respective section

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,52 +1,108 @@
 import { useState, Fragment } from 'react'
-import { useDispatch } from 'react-redux'
-import { setSelectedTab } from '../../store/navigationSlice'
 import styled from 'styled-components'
 import { IoMenu } from 'react-icons/io5'
+import { FaTimes } from 'react-icons/fa'
 import { colors } from '../../variables'
 
 const Nav = () => {
-  const dispatch = useDispatch()
   const [menuOpen, setMenuOpen] = useState(false)
-  const handleMenuClick = () => {
-    if (!menuOpen) {
-      setMenuOpen(true)
-    } else {
-      setMenuOpen(false)
-    }
-  }
+  const [menuIconHover, setMenuIconHover] = useState(false)
+  const [timesIconHover, setTimesIconHover] = useState(false)
   return (
     <Navbar>
-      <IoMenu style={menuIcon} onClick={handleMenuClick} />
-      <div>
+      {!menuOpen && <StyledIoMenu
+        onClick={() => setMenuOpen(true)}
+        onPointerOver={() => {
+            setMenuIconHover(true)
+          }}
+        onPointerOut={() => {
+          setMenuIconHover(false)
+        }}
+      />}
+      {menuOpen && <StyledFaTimes
+        onClick={() => setMenuOpen(false)}
+        onMouseEnter={() => setTimesIconHover(true)}
+        onMouseLeave={() => setTimesIconHover(false)}
+      />}
+      <Container>
         {menuOpen && (
           <Fragment>
-            <Button data-testid='about' onClick={() => dispatch(setSelectedTab('about'))}>About Me</Button>
-            <Button data-testid='projects' onClick={() => dispatch(setSelectedTab('projects'))}>Projects</Button>
-            <Button data-testid='resume' onClick={() => dispatch(setSelectedTab('resume'))}>Resume</Button>
-            <Button data-testid='contact' onClick={() => dispatch(setSelectedTab('contact'))}>Contact Me</Button>
+            <ButtonContainer><Button data-testid='about' href='#about'>About Me</Button></ButtonContainer>
+            <ButtonContainer><Button data-testid='projects' href='#projects'>Projects</Button></ButtonContainer>
+            <ButtonContainer><Button data-testid='resume' href='#resume'>Resume</Button></ButtonContainer>
+            <ButtonContainer><Button data-testid='contact' href='#contact'>Contact Me</Button></ButtonContainer>
           </Fragment>
         )}
-      </div>
+      </Container>
     </Navbar>
   )
 }
 
 const Navbar = styled.nav`
-display: flex;
-justify-content: space-between;
+display: grid;
+grid-template-columns: repeat(3, 1fr);
 background-color: ${colors.blue};
+position: relative;
+z-index: 1;
+height: 11rem;
 `
 
-const Button = styled.button`
+const Button = styled.a`
+position: relative;
 padding: 1rem;
 margin: 0.5rem 0 0.5rem 1rem;
+text-decoration: none;
+color: ${colors.green};
+background-color: ${colors.pink};
+border-radius: 10px;
+&:hover {
+  top: -0.5rem;
+  box-shadow: 0 0.5rem grey;
+}
+&:active {
+  top: -0.25rem;
+  box-shadow: 0 0.25rem grey;
+}
 `
 
-const menuIcon = {
-  color: `${colors.pink}`,
-  fontSize: '4rem',
-  cursor: 'pointer',
+const StyledIoMenu = styled(IoMenu)`
+color: ${colors.pink};
+font-size: 11rem;
+cursor: pointer;
+position: relative;
+&:hover {
+  top: -0.5rem;
+  filter: drop-shadow(0 0.5rem 0 grey);
 }
+&:active {
+  top: -0.25rem;
+  filter: drop-shadow(0 0.25rem 0 grey);
+}
+`
+
+const StyledFaTimes = styled(FaTimes)`
+color: ${colors.pink};
+font-size: 11rem;
+cursor: pointer;
+position: relative;
+&:hover {
+  top: -0.5rem;
+  filter: drop-shadow(0 0.5rem 0 grey);
+}
+&:active {
+  top: -0.25rem;
+  filter: drop-shadow(0 0.25rem 0 grey);
+}
+`
+
+const Container = styled.div`
+display: flex;
+justify-content: space-around;
+`
+
+const ButtonContainer = styled.div`
+display: flex;
+align-items: center;
+`
 
 export default Nav
